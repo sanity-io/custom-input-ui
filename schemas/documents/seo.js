b1d3639861ba2c6fd1bc4e177615fb0edcffd94b
Bryan Robinson <bryanrobinson@gmail.com>
2020-12-17 17:04:08 -0500

    initial commit

---
+++ schemas/documents/seo.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import {PatchEvent, set} from 'part:@sanity/form-builder/patch-event'
+import FormField from 'part:@sanity/components/formfields/default'
+
+import {
+  Tooltip,
+  Text,
+  Box,
+  TextInput
+} from '@sanity/ui'
+
+const HoverInput = React.forwardRef((props, ref) => {
+  const { type, onChange } = props
+  return(
+    <FormField label={type.title} description={type.description}>
+      {type.tipDescription ? 
+        <Tooltip
+          content={(
+            <Box padding={2}>
+              <Text>{type.tipDescription}</Text>
+            </Box>
+          )}
+          placement="top"
+        >
+          <TextInput
+            type="text"
+            ref={ref}
+            placeholder={type.placeholder}
+            value={props.value}
+            onChange={event => {onChange(PatchEvent.from(set(event.target.value)))}}
+          /> 
+        </Tooltip>
+      : 
+        <TextInput
+          type="text"
+          ref={ref}
+          placeholder={type.placeholder}
+          value={props.value}
+          onChange={event => {onChange(PatchEvent.from(set(event.target.value)))}}
+        /> 
+      }
+      
+    </FormField>
+  )
+
+})
+
+export default {
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'seoString',
+      title: 'Something really important for SEO',
+      description: 'Don\'t forget to make it SEO friendly!',
+      type: 'string',
+      tipDescription: 'Hey! Seriously, make it SEO friendly!',
+      inputComponent: HoverInput
+    }
+  ]
+}
